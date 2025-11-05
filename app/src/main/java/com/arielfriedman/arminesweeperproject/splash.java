f2c733d719eeb1b4e2fcf27eb43821cde4a8cd96
Student-2024 <student-2024@ashalim.org>
2025-11-05 15:54:11 +0200

    I added splash and login with music for splash

---
+++ app/src/main/java/com/arielfriedman/arminesweeperproject/splash.java
@@ -0,0 +1,54 @@
+package com.arielfriedman.arminesweeperproject;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class splash extends AppCompatActivity {
+
+    private ImageView myImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        myImageView = findViewById(R.id.imageView);
+
+        Thread mSplashThread = new Thread(){
+            @Override
+            public void run(){
+                try{
+                    synchronized (this){
+                        MediaPlayer music = MediaPlayer.create(splash.this, R.raw.elevmusic);
+                        music.start();
+                        Animation myFadeInAnimation = AnimationUtils.loadAnimation(splash.this, R.anim.tween);
+                        wait(1500);
+                    }
+                }
+                catch (InterruptedException ex){
+                }
+                finish();
+
+                Intent intent = new Intent(splash.this, login.class);
+                startActivity(intent);
+            }
+
+        };
+        mSplashThread.start();
+    }
+}
