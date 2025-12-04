d916c66fe56de59cef1b8ccb1676df857a716a32
Student-2024 <student-2024@ashalim.org>
2025-12-04 08:39:52 +0200

    added AdminPage (empty)

--- app/src/main/java/com/arielfriedman/arminesweeperproject/AdminMainActivity.java
+++ app/src/main/java/com/arielfriedman/arminesweeperproject/AdminMainActivity.java
@@ -1,8 +1,6 @@
 package com.arielfriedman.arminesweeperproject;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,25 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ActShowData extends AppCompatActivity {
-
-    TextView firstName, lastName, email, phone;
-    Intent recived;
+public class AdminMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_act_show_data);
+        setContentView(R.layout.activity_admin_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        recived = getIntent();
-        firstName = findViewById(R.id.fname);
-        lastName = findViewById(R.id.lname);
-        email = findViewById(R.id.email);
-        phone = findViewById(R.id.phone);
     }
 }
